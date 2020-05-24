5633804a0b40d08d86e9d202992d72039abe2eb0
zhanghua <1582373436@qq.com>
2020-05-24 15:21:01 +0800

    提交ci

---
+++ src/main/java/com/example/demo/controller/CommonController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import com.example.demo.pojo.Person;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CommonController {
+    @RequestMapping(value = "/a")
+    public List<Person> findAll(){
+        List persons = new ArrayList();
+        for(int i=0;i<3;i++){
+            Person person = new Person();
+            person.setId(i);
+            person.setName("p"+i);
+            persons.add(person);
+        }
+        return persons;
+    }
+}
