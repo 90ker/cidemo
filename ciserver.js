70adffe8015cf7eba5ef29dfe66ccb988c3ddce5
zhanghua <1582373436@qq.com>
2020-05-25 11:07:23 +0800

    console

--- ciserver.js
+++ ciserver.js
@@ -2,7 +2,7 @@ const http = require('http')
 const child_process = require('child_process')
 let server = http.createServer((req,res)=>{
     child_process.exec('./ci.sh',(err,out,stderr)=>{
-
+        console.log(out,err)
     })
     res.end("one push")
 });
